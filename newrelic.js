ece0777fd1f16cf6ca7c3e8a3e5eb512021e8c2c
yaboi <tyler@tylergoelz.com>
2014-12-11 17:30:24 +0000

    Remove merge message from newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -5,7 +5,6 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-<<<<<<< HEAD
   /**
    * Array of application names.
    */
@@ -23,22 +22,3 @@ exports.config = {
     level : 'info'
   }
 };
-=======
-    /**
-     * Array of application names.
-     */
-    app_name: ['Tylergoelz Staging'],
-    /**
-     * Your New Relic license key.
-     */
-    license_key: 'a64d7b9cf679af265ccf561a9950fbed22c8d15e ',
-    logging: {
-        /**
-         * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-         * issues with the agent, 'info' and higher will impose the least overhead on
-         * production applications.
-         */
-        level: 'info'
-    }
-};
->>>>>>> eb406229d699b6f260267b7bee2e401885ff7ebe
