dcb138eb0066f34d8e2b4dae39817dc8e108a60b
thinq4yourself <joel@thinq4yourself.com>
2015-01-20 14:58:08 +0000

    :rocket: crap

--- web.js
+++ web.js
@@ -4,14 +4,16 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
-// app.use(function(req, res, next) {
-//     if (req.headers['x-forwarded-proto'] != 'https') {
-//         res.redirect('https://' + req.headers.host + req.path);
-//     }
-//     else {
-//         return next();
-//     }
-// });
+ // this is used to force ssl in http based requests. 
+ // Important for security reasons.
+ app.use(function(req, res, next) {
+     if (req.headers['x-forwarded-proto'] != 'https') {
+         res.redirect('https:' + req.headers.host + req.path);
+     }
+     else {
+         return next();
+     }
+ });
 app.use(morgan('app'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
