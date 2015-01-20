d24f4ea09127ee3dd4a0abda02f94a831e686e6f
thinq4yourself <joel@thinq4yourself.com>
2015-01-20 15:18:06 +0000

    fixing the :rocket: crap commit and resetting back to , fixing gitignore and re-enstating secutity measures in web.js

--- web.js
+++ web.js
@@ -4,14 +4,15 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
-// app.use(function(req, res, next) {
-//     if (req.headers['x-forwarded-proto'] != 'https') {
-//         res.redirect('https://' + req.headers.host + req.path);
-//     }
-//     else {
-//         return next();
-//     }
-// });
+/* this is used to force SSL - required for security */
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] != 'https') {
+        res.redirect('https://' + req.headers.host + req.path);
+    }
+    else {
+        return next();
+    }
+});
 app.use(morgan('app'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
