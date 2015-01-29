6b35de7094df874b3f536e9f07abd1da4f3fce8b
yaboi <tyler@tylergoelz.com>
2015-01-29 02:06:19 +0000

    Close #27. Add uservoice snippet to index.html. Even though ugly, gets the job done. In a perfect world, it would be in a directive. Alas, this world is not perfect. Maybe one day...

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,36 +6,30 @@
  * # MainCtrl
  * Controller of the angularApp
  */
-angular.module('app.controllers', [
-
-]).controller('WriteOnCtrl', function($scope, $location) {
-    
+angular.module('app.controllers', []).controller('WriteOnCtrl', function($scope, $location) {
     function isMain() {
-        return ($location.path() === '/home') || ($location.path() === '/features') ? true : false;
+        return($location.path() === '/home') || ($location.path() === '/features') ? true : false;
     }
+
     function isInverse() {
-        return ($location.path() === '/privacy') ? true : false;
+        return($location.path() === '/privacy') ? true : false;
     }
-    
     $scope.main = isMain();
     $scope.inverse = isInverse();
     $scope.isCollapsed = true;
-    
     $scope.$on('$locationChangeStart', function() {
         $scope.main = isMain();
         $scope.inverse = isInverse();
     });
-    $scope.$on('$routeChangeSuccess', function () {
-      window.scrollTo(0, 0);
+    $scope.$on('$routeChangeSuccess', function() {
+        window.scrollTo(0, 0);
     });
     
 }).controller('MainCtrl', function($scope, $location, $routeParams) {
-    $scope.full = ($routeParams.fG7tNpKU) ? true : false;        
-        
-}).controller('NavController', function($scope, $location) {
+    $scope.full = ($routeParams.fG7tNpKU) ? true : false;
     
+}).controller('NavController', function($scope, $location) {
     $scope.go = function(url) {
         $location.path(url);
     };
-    
 });
